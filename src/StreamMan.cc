95ea71881f51fe1f244c09b09d408844a5b5bd11
Jason Wang <wangr1@ornl.gov>
2017-03-08 16:51:27 -0500

    added default numbers for tolerance and priority in StreamMan

--- src/StreamMan.cc
+++ src/StreamMan.cc
@@ -34,7 +34,9 @@ int StreamMan::init(json p_jmsg){
 
         if(p_jmsg["num_channels"] != nullptr) m_num_channels = p_jmsg["num_channels"];
         if(p_jmsg["tolerance"] != nullptr) m_tolerance = p_jmsg["tolerance"].get<vector<int>>();
+        else m_tolerance.assign(m_num_channels, 0);
         if(p_jmsg["priority"] != nullptr) m_priority = p_jmsg["priority"].get<vector<int>>();
+        else m_priority.assign(m_num_channels, 100);
 
         if(!zmq_context){
             zmq_context = zmq_ctx_new ();
